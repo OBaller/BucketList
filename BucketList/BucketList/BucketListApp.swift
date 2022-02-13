401b6702567e8fa35e8b0a31065e93df6f1426f4
brightsummit.co <naseemoyebola@gmail.com>
2022-02-13 23:19:34 +0100

    initial commit

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by naseem on 13/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
