09b0012fc1e310c21836467930a80ff6ae00c08a
brightsummit.co <naseemoyebola@gmail.com>
2022-02-14 23:47:52 +0100

    custom annotation

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -10,69 +10,55 @@ import MapKit
 import LocalAuthentication
 
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
 
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
-    
-    @State private var isUnlocked = false
-
-    
-//    func getDocumentsDirectory() -> URL {
-//        // find all possible documents directories for this user
-//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-//
-//        // just send back the first one, which ought to be the only one
-//        return paths[0]
-//    }
-//
-//    let users = [
-//            User(firstName: "Arnold", lastName: "Rimmer"),
-//            User(firstName: "Kristine", lastName: "Kochanski"),
-//            User(firstName: "David", lastName: "Lister"),
-//    ].sorted()
+    @State private var locations = [Location]()
     
     var body: some View {
         NavigationView {
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                MapAnnotation(coordinate: location.coordinate) {
-                    NavigationLink {
-                        Text(location.name)
-                    } label: {
-                        Circle()
-                            .stroke(.red, lineWidth: 3)
-                            .frame(width: 44, height: 44)
+            ZStack {
+                Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+                    MapAnnotation(coordinate: location.coordinate) {
+                        VStack {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundColor(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(Circle())
+
+                            Text(location.name)
+                        }
+                    }
+                }
+                .ignoresSafeArea()
+                Circle()
+                    .fill(.blue)
+                    .opacity(0.3)
+                    .frame(width: 32, height: 32)
+                
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button {
+                            // create a new location
+                            let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+                            locations.append(newLocation)
+                        } label: {
+                            Image(systemName: "plus")
+                        }
+                        .padding()
+                        .background(.black.opacity(0.75))
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .clipShape(Circle())
+                        .padding(.trailing)
                     }
                 }
             }
-            .navigationTitle("London Explorer")
         }
         
-    }
-    
-    func authenticate() {
-        let context = LAContext()
-        var error: NSError?
-
-        // check whether biometric authentication is possible
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            // it's possible, so go ahead and use it
-            let reason = "We need to unlock your data."
-
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
-                // authentication has now completed
-                if success {
-                    // authenticated successfully
-                    isUnlocked = true
-                } else {
-                    // there was a problem
-                }
-            }
-        } else {
-            // no biometrics
-        }
     }
 }
 
@@ -81,37 +67,3 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
-
-
-struct User: Identifiable, Comparable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-
-    static func <(lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
-
-//Text("Hello World")
-//    .onTapGesture {
-//        let str = "Test Message"
-//        let url = getDocumentsDirectory().appendingPathComponent("message.txt")
-//
-//        do {
-//            try str.write(to: url, atomically: true, encoding: .utf8)
-//            let input = try String(contentsOf: url)
-//            print(input)
-//        } catch {
-//            print(error.localizedDescription)
-//        }
-//    }
-
-
-//VStack {
-//    if isUnlocked {
-//        Text("Unlocked")
-//    } else {
-//        Text("Locked")
-//    }
-//}
