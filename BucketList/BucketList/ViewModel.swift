d2e788f60a4c0c9b640c2512b5de46ebacdf8fe4
brightsummit.co <naseemoyebola@gmail.com>
2022-02-15 14:55:04 +0100

    Persist data: FileManager

--- BucketList/BucketList/ViewModel.swift
+++ BucketList/BucketList/ViewModel.swift
@@ -10,14 +10,17 @@ import MapKit
 
 extension ContentView {
     @MainActor class ViewModel: ObservableObject {
+        let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
+
         @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
         
-        @Published private(set) var locations = [Location]()
+        @Published private(set) var locations: [Location]
         @Published var selectedPlace: Location?
         
         func addLocation() {
             let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
             locations.append(newLocation)
+            save()
         }
         
         func update(location: Location) {
@@ -25,6 +28,25 @@ extension ContentView {
 
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
+                save()
+            }
+        }
+        
+        init() {
+            do {
+                let data = try Data(contentsOf: savePath)
+                locations = try JSONDecoder().decode([Location].self, from: data)
+            } catch {
+                locations = []
+            }
+        }
+        
+        func save() {
+            do {
+                let data = try JSONEncoder().encode(locations)
+                try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+            } catch {
+                print("Unable to save data.")
             }
         }
     }
